71e299a73d8516373ceb4291c42aa57108eb6136
Thomas Lopez <Vondirath@gmail.com>
2016-07-11 14:24:04 -0600

    try

--- js/controllers/PostsController.js
+++ js/controllers/PostsController.js
@@ -1,13 +1,15 @@
 app.controller('PostsController', ['$scope','suggestions', '$routeParams', function ($scope, suggestions, $routeParams) {
 
     $scope.post = suggestions.posts[$routeParams.id];
+    
     $scope.comments = [];
-    $scope.addComment = function (comments) {
+    
+    $scope.addComment = function ( ) {
             $scope.posts.comments.push({
                 body: [],
                 upvotes: 0,
             });
-            }
+            };  
     $scope.upVote = function (comment) {
             $scope.posts[comment].upvotes += 1;
             };
